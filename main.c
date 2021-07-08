625e38a1b03330291d17317ea5a3a24d1b598d66
Nam Cao <cvn249@gmail.com>
2021-07-08 23:56:50 +0700

    Add deep sleep at the end of execution

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include "gpio.h"
+#include "power.h"
 #include "rcc.h"
 
 int main() {
     /* gpiod clock */
     RCC->AHB1ENR = (1u << 3u);
 
+    __asm__("nop");
+
     GPIOD->MODER = (1u << 24u) | (1u << 26u) | (1u << 28u) | (1u << 30u);
     GPIOD->ODR = (1u << 12u) | (1u << 13u) | (1u << 14u) | (1u << 15u);
+
+    deepsleep();
 }
